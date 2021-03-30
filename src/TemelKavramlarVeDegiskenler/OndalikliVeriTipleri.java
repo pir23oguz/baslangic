99c4067fa652f895a2d52bc46372fa80cc80f24c
Niyazi Oguz <78979256+pir23oguz@users.noreply.github.com>
2021-03-30 17:58:30 +0300

    video 10 sonu

--- src/TemelKavramlarVeDegiskenler/OndalikliVeriTipleri.java
+++ src/TemelKavramlarVeDegiskenler/OndalikliVeriTipleri.java
@@ -5,6 +5,30 @@ public class OndalikliVeriTipleri {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		// float sayi1 = 34.0f;  doble dan ayýrmasý icin sonuna f F yazilmali
+		// float sayi1 = 34.0f;
+		// float sayi1 = 34;
+		float sayi1 = 34.5F;
+		float sayi2 = 35.1f;
+		float sayi3 = 36;
+		
+		
+		System.out.println(sayi1);
+		System.out.println(sayi2);
+		System.out.println(sayi3);
+		
+		double ondaliklisayi1 = 3;
+		double ondaliklisayi2 = 3.14;
+		System.out.println(ondaliklisayi1);
+
+	    ondaliklisayi1 = 54.0;
+
+		System.out.println(ondaliklisayi1);
+		System.out.println(ondaliklisayi2);
+
+		
+		
+		
 
 	}
 
