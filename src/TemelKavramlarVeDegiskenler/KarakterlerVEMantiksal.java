5897e0dc218986dea6ea3a6a34f1983a6d389ecf
Niyazi Oguz <78979256+pir23oguz@users.noreply.github.com>
2021-03-30 18:27:25 +0300

    12. video sonu

---
+++ src/TemelKavramlarVeDegiskenler/KarakterlerVEMantiksal.java
@@ -0,0 +1,21 @@
+package TemelKavramlarVeDegiskenler;
+
+
+public class KarakterlerVEMantiksal {
+
+	public static void main(String[] args) {
+	char karakter =	117;
+	char karakter2 = 'u'; // Tek tirnak
+	System.out.println(karakter2);
+	System.out.println("Hello World!"); // Cift tirnak
+	
+	boolean mantik = true;
+	boolean mantik2 = false;
+	// boolean mantik3 = 123;  error sart method ... kullanilir
+	// boolean karakter = false; karakter kullanýlmýs error
+	System.out.println(mantik);
+	System.out.println(mantik2);
+	
+	}
+
+}
